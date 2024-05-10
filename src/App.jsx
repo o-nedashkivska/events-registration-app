6e12d146eb7c39827825da5d9a062ea4f3324549
Oleksandra Nedashkivska <alexandranedashkovskaya10@gmail.com>
2024-05-10 15:01:27 +0300

    add support for HomePage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,18 @@
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 import RootLayout from "./layouts/Root";
 import ErrorPage from "./pages/Events";
+import HomePage from "./pages/Home";
 import EventsPage from "./pages/Events";
 import ParticipantsPage from "./pages/Participants";
 import RegistrationPage from "./pages/Registration";
 
 const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RootLayout />,
+    errorElement: <ErrorPage />,
+    children: [{ index: true, element: <HomePage /> }],
+  },
   {
     path: "/events",
     element: <RootLayout />,
